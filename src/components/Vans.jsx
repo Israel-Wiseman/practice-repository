bc87b4fe73cf8b1cb5608a71ed2d96f8f52eda33
Israel-Wiseman <138420947+Israel-Wiseman@users.noreply.github.com>
2023-07-03 15:58:15 +0300

    Adding db and rendering

--- src/components/Vans.jsx
+++ src/components/Vans.jsx
@@ -1,6 +1,45 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function Vans() {
-    return <div>
-        <h1>Vans page goes here</h1>
-    </div>
+
+    const [vans, setVans] = useState([]);
+
+    useEffect(() => {
+        fetchVans();
+
+    }, []);
+
+    const fetchVans = () => {
+        axios
+            .get(`http://localhost:3200/vans`)
+            .then((response) => {
+                setVans(response.data);
+            })
+            .catch((error) => {
+                console.error("Error fetching vans", error);
+            });
+            return vans
+    };
+
+    return (
+       
+        <div>
+            <h1>Vans list:</h1> <br></br>
+            {vans.map((van) =>(
+               <div key={van.id}>
+               <div>{van.name}</div>
+               <div>{van.price}</div>
+               <div>{van.description}</div>
+               <img src={van.imageUrl} alt={van.name} />
+               <div>{van.type}</div>
+             </div>
+
+            ) )}
+        </div>
+    )
+
+
+
 }
 export default Vans;
