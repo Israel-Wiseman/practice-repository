5d8fec39b2b9a4fd8f3256502a9f1af88495a72a
Israel-Wiseman <138420947+Israel-Wiseman@users.noreply.github.com>
2023-07-03 09:42:26 +0300

    Adding routes and links

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Styles from './App.module.css';
+import VansList from './components/VansList';
+import Vans from './components/Vans';
 
 function App() {
 
@@ -11,11 +13,15 @@ function App() {
     <nav className={Styles.navBar}>
       <Link to='/'>Home</Link>
       <Link to='/about'>About</Link>
+      <Link to='/vansList'> Vans List </Link>
+      <Link to='/vans'> Vans </Link>
     </nav>
 
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/vansList' element={<VansList/>} />
+        <Route path='/vans' element={<Vans/>} />
       </Routes>
     </div>
 
---
+++ src/components/Vans.jsx
@@ -0,0 +1,6 @@
+function Vans() {
+    return <div>
+        <h1>Vans page goes here</h1>
+    </div>
+}
+export default Vans;
